cc337670f7a57bc5cd41015b6586e943dfdf348a
vali-snow <vali-snow@users.noreply.github.com>
2018-05-26 14:20:06 +0300

    fixed missing response.success check

--- js/organize.js
+++ js/organize.js
@@ -8,35 +8,38 @@ $(document).ready(function () {
     })
     .done(function (jsonObj) {
         var response = JSON.parse(jsonObj);
-        $("#organize table tr").remove();
-		$("#organize table")
-			.append($('<tr>')
-				.append($('<th>'))
-				.append($('<th>',{text:'Name'}))
-				.append($('<th>',{text:'Description'}))
-				.append($('<th>',{text:(Cookies.get("readerId")==1)?'Manage':'Link'})));
-			if (Cookies.get("readerId")==1) {
-				$("#organize table th:first")
-					.append($('<i>', {class:"fas fa-plus"}))
-			}
-		
-		$.each(response.feeds, function (i, item) {
+		if (response.success) {
+			$("#organize table tr").remove();
 			$("#organize table")
-			.append($('<tr>',{id:item.FeedID})
-				.append($('<td>')
-					.append($('<input>',{type:'checkbox',checked:item.subscribed})))
-				.append($('<td>',{text:item.Name}))
-				.append($('<td>',{text:item.Description}))
-				.append($('<td>')
-					.append($('<a>',{href:item.Link})
-						.append($('<i>', {class:"fas fa-link"})))));
-			if (Cookies.get("readerId")==1) {
-				$("#organize table td:last")
-					.append($('<i>', {class:"fas fa-edit"}))
-					.append($('<i>', {class:"fas fa-eraser"}));
-			}
-		});
+				.append($('<tr>')
+					.append($('<th>'))
+					.append($('<th>',{text:'Name'}))
+					.append($('<th>',{text:'Description'}))
+					.append($('<th>',{text:(Cookies.get("readerId")==1)?'Manage':'Link'})));
+				if (Cookies.get("readerId")==1) {
+					$("#organize table th:first")
+						.append($('<i>', {class:"fas fa-plus"}))
+				}
 			
+			$.each(response.feeds, function (i, item) {
+				$("#organize table")
+				.append($('<tr>',{id:item.FeedID})
+					.append($('<td>')
+						.append($('<input>',{type:'checkbox',checked:item.subscribed})))
+					.append($('<td>',{text:item.Name}))
+					.append($('<td>',{text:item.Description}))
+					.append($('<td>')
+						.append($('<a>',{href:item.Link})
+							.append($('<i>', {class:"fas fa-link"})))));
+				if (Cookies.get("readerId")==1) {
+					$("#organize table td:last")
+						.append($('<i>', {class:"fas fa-edit"}))
+						.append($('<i>', {class:"fas fa-eraser"}));
+				}
+			});
+		} else {
+			window.alert("Unable to read the feeds!");
+		}
     })
     .fail(function (response) {
         console.log('Error while getting all feeds details:\n' + response.status + ' - ' + response.statusText);
